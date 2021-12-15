4465f6ad5daf23cb2458b5b7e121dfa307f6b5ba
Rodrigo Martins Cirqueira <rodrigo.cirqueira@ambevtech.com.br>
2021-12-15 10:45:18 -0300

    Fluent

---
+++ TesteUnidade/Demo/Features.Tests/07 - FluentAssertions/ClientFluentAssertionsTests.cs
@@ -0,0 +1,45 @@
+﻿using Features.Clientes;
+using Features.Tests._02___Fixtures;
+using FluentAssertions;
+using Moq;
+using Moq.AutoMock;
+using System.Linq;
+using Xunit;
+
+namespace Features.Tests._07___FluentAssertions
+{
+    [Collection(nameof(ClienteCollection))]
+    public class ClientFluentAssertionsTests
+    {
+        private readonly ClienteTestsFixture _clienteTestsFixture;
+
+        public ClientFluentAssertionsTests(ClienteTestsFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            var mocker = new AutoMocker();
+            var clienteService = mocker.CreateInstance<ClienteService>();
+
+            mocker.GetMock<IClienteRepository>().Setup(c => c.ObterTodos())
+                .Returns(_clienteTestsFixture.GerarClientesVariados());
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+            // Assert 
+            mocker.GetMock<IClienteRepository>().Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
+
+            // Assert 
+            mocker.GetMock<IClienteRepository>().Verify(r => r.ObterTodos(), Times.Once);
+            clientes.Any().Should().BeTrue();
+            clientes.Where(w => !w.Ativo).Should().HaveCountGreaterThan(0);
+        }
+    }
+}
